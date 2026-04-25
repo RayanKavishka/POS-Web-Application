2684249e53360f576a5af9f53eb9c4ee1cba9c24
rayankavishka <rayanpersonaluse2004@gmail.com>
2026-04-25 08:44:23 +0530

    The project structure was implemented

--- utill/regexUtill.js
+++ utill/regexUtill.js
@@ -1,13 +1,8 @@
-// ------------------------ Student Regex --------------------------
-const nic_regex = new RegExp("^(([5,6,7,8,9]{1})([0-9]{1})([0,1,2,3,5,6,7,8]{1})([0-9]{6})([v|V|x|X]))|(([1,2]{1})([0,9]{1})([0-9]{2})([0,1,2,3,5,6,7,8]{1})([0-9]{7}))");
+// ------------------------ Customer Regex --------------------------
 const phone_regex = new RegExp("^[0]{1}[7]{1}[01245678]{1}[0-9]{7}$");
-
-const checkNic = (nic) => {
-    return nic_regex.test(nic);
-};
 
 const checkPhone = (phone) => {
     return phone_regex.test(phone);
 };
 
-export {checkNic, checkPhone};
+export {checkPhone};
