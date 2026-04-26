a974e272463b84dc7e4309a2e1250a6a615b6753
rayankavishka <rayanpersonaluse2004@gmail.com>
2026-04-26 10:43:49 +0530

    completed sidebar controller

--- controller/SidebarController.js
+++ controller/SidebarController.js
@@ -1,3 +1,4 @@
+// Side Bar Showing Logic
 let isActive = true;
 
 $('header button').on('click', function () {
@@ -9,5 +10,30 @@ $('header button').on('click', function () {
     } else {
         $('#sidebar').removeClass('d-flex').css({display: 'none'});
         $('.content').css({marginLeft: '15px'});
+    }
+});
+
+
+// Side Bar Navigation
+$('aside button').on('click', function () {
+    $('.content').hide();
+
+    if ($(this).text() === "Dashboard") {
+        $('#dashboardSection').show();
+
+    } else if ($(this).text() === "Place Order") {
+        $('#placeOrderSection').show();
+
+    } else if ($(this).text() === "Customers") {
+        $('#customerSection').show();
+
+    } else if ($(this).text() === "Items") {
+        $('#ItemSection').show();
+
+    }  else if ($(this).text() === "Order History") {
+        $('#OrderHistorySection').show();
+
+    } else {
+        window.location.replace('login.html');
     }
 });
